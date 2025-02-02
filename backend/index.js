37eb80f5da2045115471ee77048fc11272b83631
topograp2 <topo.or.dubu@gmail.com>
2025-02-02 20:28:04 +0900

    feat: 로그아웃 및 회원탈퇴 구현

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,8 @@ app.use(cors({
     credentials: true
 }
 ));
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
