503e12eb0e5456a6080ae578baf7ecc9b8fbbec7
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-02-02 20:41:42 +0900

    Revert "week 1~2: 로그인, 회원가입 기능 구현"

--- frontend/src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-const Logo = () => {
-  return (
-    <div className="w-full flex items-center mt-10 flex-col gap-2">
-      <img className="size-15"src="/logo.png" alt="로고" />
-      <div className="bg-white w-80 h-px"></div>
-    </div>
-  );
-};
-export default Logo;
